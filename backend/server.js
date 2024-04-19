40d4512af3f38d15752fa79ad15c1b4f68efd019
Hussainshabdi <sh9619192127@gmail.com>
2024-04-19 12:35:09 -0700

    second commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express=require('express');
+// import express = require('express');
+const dotenv =require('dotenv');
+const morgan =require('morgan');
+const colors =require('colors');
+
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'))
+
+//routes
+app.get('/',(req,res)=>{
+    res.status(200).send({
+        message:"this port is running"
+    })
+})
+
+
+//port
+const port = process.env.PORT || 8080;
+
+//listen
+app.listen(port,()=>{
+    console.log("port is running")
+})
